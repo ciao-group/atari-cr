5dc6882b4164119950da555c12aecd52e01c3de8
Niko <be66akax@studserv.uni-leipzig.de>
2024-07-04 10:18:05 +0200

    v1.10: Increase pause cost from 0.01 to 0.02

--- src/atari_cr/common/pauseable_env.py
+++ src/atari_cr/common/pauseable_env.py
@@ -8,7 +8,7 @@ import numpy as np
 
 # Small cost for a vision step without actual env step 
 # to prevent the model from abusing only vision steps
-PAUSE_COST = 0.01
+PAUSE_COST = 0.02
 
 class PauseableFixedFovealEnv(FixedFovealEnv):
     """
