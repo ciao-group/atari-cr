e286e827fa5b0bed038d6801663539c38e2dd218
Niko <be66akax@studserv.uni-leipzig.de>
2024-07-16 20:13:08 +0200

    v1.21 - Increase successive pause limit to 30 - Increase pause cost from 0.02 to 0.025

--- start_training.sh
+++ start_training.sh
@@ -5,8 +5,8 @@ numgpus=${2:-$(nvidia-smi --list-gpus | wc -l)}
 #envlist=(alien amidar assault asterix bank_heist battle_zone boxing breakout chopper_command crazy_climber demon_attack freeway frostbite gopher hero jamesbond kangaroo krull kung_fu_master ms_pacman pong private_eye qbert road_runner seaquest up_n_down) #pong qbert seaquest zaxxon
 envlist=(boxing)
 
-expname="boxing_pauseable120_2m"
-totaltimesteps="2000000"
+expname="boxing_pauseable121_3m"
+totaltimesteps="3000000"
 buffersize="100000"
 learningstarts="10000"
 
@@ -25,20 +25,21 @@ do
             echo "${expname} GPU: ${gpuid} Env: ${envlist[$i]} Seed: ${seed} ${1}"
             basename=$(basename $1)
             echo "========" >> ${logdir}/${envlist[$i]}__${basename}__${seed}.txt
-            CUDA_VISIBLE_DEVICES=$gpuid python $1 --env ${envlist[$i]} \
-                                                  --env-num 1 \
-                                                  --seed $seed \
-                                                  --exp-name ${expname} \
-                                                  --fov-size 20 \
-                                                  --clip-reward \
-                                                  --capture-video \
-                                                  --total-timesteps $totaltimesteps \
-                                                  --buffer-size $buffersize \
-                                                  --learning-start $learningstarts \
-                                                  --pvm-stack 6 \
-                                                  --pause-cost 0.02 \
-                                                  --successive-pause-limit 20 \
-                                                  ${@:2} >> ${logdir}/${envlist[$i]}__${basename}__${seed}.txt
+            CUDA_VISIBLE_DEVICES=$gpuid python $1 \
+                --env ${envlist[$i]} \
+                --env-num 1 \
+                --seed $seed \
+                --exp-name ${expname} \
+                --fov-size 20 \
+                --clip-reward \
+                --capture-video \
+                --total-timesteps $totaltimesteps \
+                --buffer-size $buffersize \
+                --learning-start $learningstarts \
+                --pvm-stack 6 \
+                --pause-cost 0.025 \
+                --successive-pause-limit 30 \
+                ${@:2} >> ${logdir}/${envlist[$i]}__${basename}__${seed}.txt
         done
     ) &
 done
