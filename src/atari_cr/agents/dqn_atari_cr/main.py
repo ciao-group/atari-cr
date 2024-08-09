07cd37687d2f295f8960ddb56208b441b7f36dfd
Niko <be66akax@studserv.uni-leipzig.de>
2024-08-09 15:48:53 +0200

    Add typed argument parser (WIP)

--- src/atari_cr/agents/dqn_atari_cr/main.py
+++ src/atari_cr/agents/dqn_atari_cr/main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from typing import Literal
 from distutils.util import strtobool
 
 os.environ["OMP_NUM_THREADS"] = "1"
@@ -10,6 +11,7 @@ warnings.filterwarnings("ignore", category=UserWarning)
 import gymnasium as gym
 import torch
 from torch.utils.tensorboard import SummaryWriter
+from tap import Tap
 
 from active_gym.atari_env import AtariEnv, AtariEnvArgs, AtariFixedFovealEnv
 
@@ -31,8 +33,64 @@ from atari_cr.agents.dqn_atari_cr.crdqn import CRDQN
 # TODO: (wenn nichts mehr klappt) Die Anzahl der Pausen pro Bild zb auf 4 festlegen
 # TODO: (wenn nichts mehr klappt) 
 
-# TODO: Bis 1.4 gab es gute rewards, bis 1.5 gab es Farbe im Tensorboard; gucken woran hats gelejen
+# TODO: Check v1.28 results
+# TODO: Typed argument parser
 
+class ArgParser(Tap):
+    exp_name: str = os.path.basename(__file__).rstrip(".py") # The name of this experiment
+    seed: int = 0 # The seed of the experiment
+    # disable_cuda: bool # Whether to force using CPU 
+    # TODO: Merge that with no_video
+    capture_video: bool # Whether to capture videos of the agent performances (check out `videos` folder)
+
+    # Env settings
+    env: str = "boxing" # The ID of the environment
+    env_num: int = 1 # The number of envs to train in parallel
+    fram_stack: int = 4 # The number of frames making up one observation
+    action_repeat: int = 4 # The number of times an action is repeated
+    clip_reward: bool # Whether to clip rewards
+
+    # Fovea settings
+    fov_size: int = 20 # The size of the fovea
+    fov_init_loc: int = 0 # Where to initialize the fovea
+    sensory_action_mode: Literal["absolute", "relative"] = "absolute" # Whether to interprate sensory actions absolutely or relatively
+    sensory_action_space: int = 10 # Maximum size of pixels to move the fovea in one relative sensory step. Ignored for absolute sensory action mode
+    # TODO: What is this?
+    resize_to_full: bool = False
+    sensory_action_x_size: int = 4 # How many smallest sensory steps fit in x direction
+    sensory_action_y_size: int = 4 # How many smallest sensory steps fit in y direction
+    pvm_stack: int = 3 # How many normal observation to aggregate in the PVM buffer
+
+    # Algorithm specific arguments
+    total_timesteps: int = 3000000 # The number of timesteps
+    learning_rate: float = 1e-4 # The learning rate
+    buffer_size: int = 100000 # The size of the replay buffer
+    gamma: float = 0.99 # The discount factor gamma
+    target_network_frequency: int = 1000 # How many timesteps to wait before updating the target Q network
+    batch_size: int = 32 # The batch size during training
+    start_e: float = 1.0 # The starting value for the exploration probability epsilon
+    end_e: float = 0.01 # The final value for the exploration probability epsilon
+    exploration_fraction: float = 0.10 # The fraction of `total-timesteps` it takes from until the final value of the exploration probability epsilon
+    learning_start: int = 80000 # How many timesteps to wait before training starts on the replay buffer
+    train_frequency: int = 4 # How many steps to take in the env before a new training iteration
+
+    # Eval args
+    eval_frequency: int = -1 # How many steps to take in the env before a new evaluation
+    eval_num: int = 10 # How envs are created for evaluation
+
+    # Pause args
+    use_pause_env: bool # Whether to use an env that lets the agent pause for only making a sensory action
+    pause_cost: float = 0.1 # The cost for the env to only take a sensory step
+    successive_pause_limit: int = 20 # The maximum number of successive pauses before pauses are forbidden. This prevents the agent from halting
+    no_action_pause_cost: float = 0.1 # The additional cost of pausing without performing a sensory action
+
+    # Misc
+    ignore_sugarl: bool # Whether to ignore the sugarl term for Q network learning
+    grokfast: bool # Whether to use grokfast
+    disable_tensorboard: bool # Whether to disable tensorboard
+    no_model_output: bool # Whether to disable saving the finished model
+    no_video_output: bool # Whether to disble video output of the final agent acting in the env
+    no_pvm_visualization: bool # Whether to disable output of visualizations of the content of the PVM buffer
 
 def parse_args():
     # fmt: off
