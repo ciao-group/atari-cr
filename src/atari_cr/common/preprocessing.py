0e03d1d0ee1bd58436dc03135171fa093df546ac
Niko <be66akax@studserv.uni-leipzig.de>
2024-08-14 11:06:58 +0200

    Remove copied stable baselines code from common.preprocessing

--- src/atari_cr/common/preprocessing.py
+++ src/atari_cr/common/preprocessing.py
@@ -2,6 +2,8 @@ from typing import Union, Dict
 import numpy as np
 from gymnasium import spaces
 
+import stable_baselines3.common.preprocessing as sb_preprocessing
+
 # Taken from stable_baselines3.common.preprocessing
 def get_action_dim(action_space: spaces.Space) -> Union[int, Dict]:
     """
@@ -10,25 +12,10 @@ def get_action_dim(action_space: spaces.Space) -> Union[int, Dict]:
     :param action_space:
     :return:
     """
-    if isinstance(action_space, spaces.Box):
-        return int(np.prod(action_space.shape))
-    elif isinstance(action_space, spaces.Discrete):
-        # Action is an int
-        return 1
-    elif isinstance(action_space, spaces.MultiDiscrete):
-        # Number of discrete actions
-        return int(len(action_space.nvec))
-    elif isinstance(action_space, spaces.MultiBinary):
-        # Number of binary actions
-        assert isinstance(
-            action_space.n, int
-        ), f"Multi-dimensional MultiBinary({action_space.n}) action space is not supported. You can flatten it instead."
-        return int(action_space.n)
-    # NEW
-    elif isinstance(action_space, spaces.Dict):
+    if isinstance(action_space, spaces.Dict):
         action_dims = []
         for key in action_space: 
             action_dims.append(get_action_dim(action_space[key]))
         return action_dims
     else:
-        raise NotImplementedError(f"{action_space} action space is not supported")
+        return sb_preprocessing.get_action_dim(action_space)
