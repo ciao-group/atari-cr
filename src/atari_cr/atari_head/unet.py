fa5fcb05f1331d730cf21c663f05ff437602093e
Niko <be66akax@studserv.uni-leipzig.de>
2024-10-18 18:04:32 +0200

    Remove torch.compile

--- src/atari_cr/atari_head/unet.py
+++ src/atari_cr/atari_head/unet.py
@@ -14,13 +14,11 @@ class DoubleConv(nn.Module):
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
             nn.Dropout(dropout),
-            torch.compile(nn.Conv2d(
-                in_channels, mid_channels, kernel_size=3, padding=1, bias=False)),
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
             nn.Dropout(dropout),
-            torch.compile(nn.Conv2d(
-                mid_channels, out_channels, kernel_size=3, padding=1, bias=False)),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
