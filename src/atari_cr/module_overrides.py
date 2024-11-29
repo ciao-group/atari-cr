ee145b01cc29f2e00183676ff75e89f6b14769a0
Niko <be66akax@studserv.uni-leipzig.de>
2024-11-29 11:20:02 +0100

    Add own version of FixedFovealEnv

--- src/atari_cr/module_overrides.py
+++ src/atari_cr/module_overrides.py
@@ -1,10 +1,14 @@
 from functools import partial
-from typing import Callable, Optional, Union, Dict
+from typing import Callable, Optional, Tuple, Union, Dict
+import numpy as np
 from torch import nn
 import torch
 from torchvision.models.vision_transformer import VisionTransformer
+from torchvision.transforms import Resize
 from tqdm import tqdm
+import gymnasium as gym
 from gymnasium import spaces
+from gymnasium.spaces import Box
 
 import stable_baselines3.common.preprocessing as sb_preprocessing
 
@@ -87,3 +91,136 @@ def get_action_dim(action_space: spaces.Space) -> Union[int, Dict]:
         return action_dims
     else:
         return sb_preprocessing.get_action_dim(action_space)
+
+class FixedFovealEnv(gym.Wrapper):
+    def __init__(self, env: gym.Env, args):
+        super().__init__(env)
+        self.fov_size: Tuple[int, int] = args.fov_size
+        self.fov_init_loc: Tuple[int, int] = args.fov_init_loc
+        assert (np.array(self.fov_size) < np.array(self.obs_size)).all()
+
+        self.sensory_action_mode: str = args.sensory_action_mode # "absolute","relative"
+        if self.sensory_action_mode == "relative":
+            self.sensory_action_space = np.array(args.sensory_action_space)
+        elif self.sensory_action_mode == "absolute":
+            self.sensory_action_space = np.array(self.obs_size) \
+                                        - np.array(self.fov_size)
+
+        self.resize: Resize = Resize(self.env.obs_size) if args.resize_to_full else None
+
+        self.mask_out: bool = args.mask_out
+
+        # set gym.Env attribute
+        self.action_space = Dict({
+            "motor_action": self.env.action_space,
+            "sensory_action": Box(low=self.sensory_action_space[0],
+                                 high=self.sensory_action_space[1], dtype=int),
+        })
+
+        if args.mask_out:
+            self.observation_space = Box(low=-1., high=1.,
+                                         shape=(self.env.frame_stack,)+self.env.obs_size,
+                                         dtype=np.float32)
+        elif args.resize_to_full:
+            self.observation_space = Box(low=-1., high=1.,
+                                         shape=(self.env.frame_stack,)+self.env.obs_size,
+                                         dtype=np.float32)
+        else:
+            self.observation_space = Box(low=-1., high=1.,
+                                         shape=(self.env.frame_stack,)+self.fov_size,
+                                         dtype=np.float32)
+
+        # init fov location
+        # The location of the upper left corner of the fov image,
+        # on the original observation plane
+        self.fov_loc: np.ndarray = np.empty_like(self.fov_init_loc)  #
+        self._init_fov_loc()
+
+    def _init_fov_loc(self):
+        self.fov_loc = np.rint(np.array(self.fov_init_loc, copy=True)).astype(np.int32)
+
+    def reset_record_buffer(self):
+        self.env.record_buffer["fov_size"] = self.fov_size
+        self.env.record_buffer["fov_loc"] = []
+
+    def reset(self):
+        full_state, info = self.env.reset()
+        self._init_fov_loc()
+        fov_state = self._get_fov_state(full_state)
+        info["fov_loc"] = self.fov_loc.copy()
+        if self.env.record:
+            self.reset_record_buffer()
+            self.save_transition(info["fov_loc"])
+        return fov_state, info
+
+    def _clip_to_valid_fov(self, loc):
+        return np.rint(np.clip(
+            loc, 0, np.array(self.env.obs_size) - np.array(self.fov_size))).astype(int)
+
+    def _clip_to_valid_sensory_action_space(self, action):
+        return np.rint(np.clip(action, *self.sensory_action_space)).astype(int)
+
+    def _get_fov_state(self, full_state):
+        fov_state = full_state[..., self.fov_loc[0]:self.fov_loc[0]+self.fov_size[0],
+                                    self.fov_loc[1]:self.fov_loc[1]+self.fov_size[1]]
+
+        if self.mask_out:
+            mask = np.zeros_like(full_state)
+            mask[..., self.fov_loc[0]:self.fov_loc[0]+self.fov_size[0],
+                    self.fov_loc[1]:self.fov_loc[1]+self.fov_size[1]] = fov_state
+            fov_state = mask
+        elif self.resize:
+            fov_state = self.resize(torch.from_numpy(fov_state))
+            fov_state = fov_state.numpy()
+
+        return fov_state
+
+    def _fov_step(self, full_state, action):
+        if type(action) is torch.Tensor:
+            action = action.detach().cpu().numpy()
+        elif type(action) is Tuple:
+            action = np.array(action)
+
+        if self.sensory_action_mode == "absolute":
+            action = self._clip_to_valid_fov(action)
+            self.fov_loc = action
+        elif self.sensory_action_mode == "relative":
+            action = self._clip_to_valid_sensory_action_space(action)
+            fov_loc = self.fov_loc + action
+            self.fov_loc = self._clip_to_valid_fov(fov_loc)
+
+        fov_state = self._get_fov_state(full_state)
+
+        return fov_state
+
+    def save_transition(self, fov_loc):
+        # print ("saving one transition")
+        self.env.record_buffer["fov_loc"].append(fov_loc)
+
+    def step(self, action):
+        """
+        action : {"motor_action":
+                  "sensory_action": }
+        """
+        # print ("in env", action, action["motor_action"], action["sensory_action"])
+        state, reward, done, truncated, info = \
+            self.env.step(action=action["motor_action"])
+        fov_state = self._fov_step(full_state=state, action=action["sensory_action"])
+        info["fov_loc"] = self.fov_loc.copy()
+        if self.record:
+            if not done:
+                self.save_transition(info["fov_loc"])
+        return fov_state, reward, done, truncated, info
+
+    @property
+    def unwrapped(self):
+        """
+        Grabs unwrapped environment
+
+        Returns:
+            env (MujocoEnv): Unwrapped environment
+        """
+        if hasattr(self.env, "unwrapped"):
+            return self.env.unwrapped
+        else:
+            return self.env
