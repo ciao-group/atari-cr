- Back to pixels between 0 and 1 for max pvm - Remove action masking inside QNetwork - Fix transformer_to_proper_csv for polars

src/atari_cr/agents/dqn_atari_cr/crdqn.py
@@ -156,13 +156,11 @@ class CRDQN:
 
         # Q networks
         self.q_network = QNetwork(
-            self.env, len(self.sensory_action_set), pause_feat, s_action_feat,
-            self.env.envs[0].pause_action
+            self.env, len(self.sensory_action_set), pause_feat, s_action_feat
         ).to(self.device)
         self.optimizer = Adam(self.q_network.parameters(), lr=learning_rate)
         self.target_network = QNetwork(
-            self.env, len(self.sensory_action_set), pause_feat, s_action_feat,
-            self.env.envs[0].pause_action
+            self.env, len(self.sensory_action_set), pause_feat, s_action_feat
         ).to(self.device)
         self.target_network.load_state_dict(self.q_network.state_dict())
 
@@ -219,6 +217,7 @@ class CRDQN:
                 latest_model = seeded_models[np.argmax(timesteps)]
                 self.load_checkpoint(f"{self.model_dir}/{latest_model}")
                 n += self.timestep
+                self.learning_start += self.timestep
 
         # Start acting in the environment
         self.start_time = time.time()

src/atari_cr/agents/dqn_atari_cr/networks.py
@@ -10,11 +10,10 @@ from gymnasium import spaces
 
 class QNetwork(nn.Module):
     def __init__(self, env, sensory_out_dim: int, pause_feat: bool,
-                 s_action_feat: bool, pause_action: int):
+                 s_action_feat: bool):
         super().__init__()
         self.pause_feat = pause_feat
         self.s_action_feat = s_action_feat
-        self.pause_action = pause_action
 
         # Get the size of the different network heads
         assert isinstance(env.single_action_space, spaces.Dict)
@@ -96,10 +95,6 @@ class QNetwork(nn.Module):
                 consecutive_pauses, prev_pause_actions)
 
             motor_actions = torch.argmax(motor_q_values, dim=1).cpu().numpy()
-
-            # # Mask out no action pauses where the fovea does not move
-            # pause_inds = np.where(motor_actions == self.pause_action)[0]
-            # sensory_q_values[pause_inds, sensory_noops] = torch.finfo(torch.float32).min
 
             sensory_actions = torch.argmax(sensory_q_values, dim=1).cpu().numpy()
 

src/atari_cr/foveation.py
@@ -160,7 +160,7 @@ class Fovea():
                     visual_info = float(((w * h) + 15 * (self.size[0] * self.size[1]))
                         / (16 * w * h))
                 else: # Fill the area outside the crop with zeros
-                    masked_state = np.full_like(img_stack, 0.5)
+                    masked_state = np.full_like(img_stack, 0.5 if self.weighting else 0)
                     visual_info = float((self.size[0] * self.size[1]) / (w * h))
 
                 for fixation in fixations:
@@ -194,7 +194,6 @@ class Fovea():
             case "exponential":
                 visual_infos = np.empty(len(img_stack))
                 for i, img in enumerate(img_stack):
-                    # Scale images between -1 and 1
                     img = (img[...,None] * 255).astype(np.uint8)
                     img, resolutions = foveat_img(img.copy(), fixations)
                     visual_info = resolutions.mean().item()