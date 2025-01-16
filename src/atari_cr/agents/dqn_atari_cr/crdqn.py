- Remove windowed AUC - Fix duration calculation over all eval envs

src/atari_cr/agents/dqn_atari_cr/crdqn.py
@@ -194,8 +194,6 @@ class CRDQN:
             pvm_stack, (self.n_envs, frame_stack, *self.obs_size), mean_pvm=mean_pvm)
 
         self.auc = 0.6
-        self.auc_window = deque(maxlen=5)
-        self.windowed_auc = self.auc
 
     def learn(self, n: int, experiment_name: str):
         """
@@ -307,7 +305,7 @@ class CRDQN:
         # Set networks to eval mode
         self.sfn.eval()
 
-        episode_infos, out_paths, aucs, emma_times = [], [], [], []
+        episode_infos, out_paths, aucs, emma_times, duration_infos = [], [], [], [], []
         for eval_ep in range(self.n_evals):
             # Create env
             eval_env = self.eval_env_generator(eval_ep) # VecEnv with a single env in it
@@ -404,7 +402,6 @@ class CRDQN:
                         self.model_dir, "pt", self.save_checkpoint, eval_ep)
 
             # AUC calculation
-            duration_info = None
             episode_record = single_eval_env.prev_episode \
                 if isinstance(single_eval_env, PauseableFixedFovealEnv) \
                 else EpisodeRecord.from_record_buffer(
@@ -417,20 +414,17 @@ class CRDQN:
                     loader = dataset.to_loader()
                     aucs.append(self.evaluator.eval(loader)["auc"])
                 # Duration error calculation
-                # TODO: duration_info is only logged for the last eval env, this should
-                # be aggregated
-                duration_info = DurationInfo.from_episodes(
-                    [episode_record], self.env_name)
+                duration_infos.append(
+                    DurationInfo.from_episodes([episode_record], self.env_name))
 
             emma_times.extend(episode_record.annotations["emma_time"].drop_nulls().to_list())
 
             eval_env.close()
 
-        # AUC and windowed AUC
+        # AUC duration info
         if self.evaluator:
             if aucs: self.auc = sum(aucs) / len(aucs)
-            self.auc_window.append(self.auc)
-            self.windowed_auc = sum(self.auc_window) / len(self.auc_window)
+        duration_info = DurationInfo(np.concatenate(duration_infos), self.env_name)
 
         # Log mean result of eval episodes
         mean_episode_info: EpisodeInfo = \
@@ -546,7 +540,6 @@ class CRDQN:
             "sfn_loss": self.sfn_loss.item() if hasattr(self, "sfn_loss") else None,
             "timestep": self.timestep,
             "auc": self.auc,
-            "windowed_auc": self.windowed_auc,
             "truncated": episode_info["truncated"],
             "eval_env": eval_env,
             "emma_times": emma_times

src/atari_cr/models.py
@@ -280,20 +280,39 @@ class TdUpdateInfo(NamedTuple):
     sensory_target_max: float
 
 class DurationInfo:
-    """ Durations in ms """
-    error: float
-    mean: float
-    median: float
-    hist: list[float]
-    durations: list[float]
+    """
+    Info about the gaze durations
 
-    def __init__(self, error: float, mean: float, median: float, hist: list[float],
-                 durations: list[float]):
-        self.error = error
-        self.mean = mean
-        self.median = median
-        self.hist = hist
-        self.durations = durations
+    Attributes:
+        error (float): Wasserstein distance between the distribution of durations in the
+            episode vs. the distribution observed in human data.
+        durations (Array[N;f32]): List of all gaze durations in one episode in ms.
+    """
+    error: float
+    durations: np.ndarray
+
+    def __init__(self, durations: list[float], env_name: str):
+        self._durations = np.array(durations)
+        self._env_name = env_name
+        self._error = None # Lazily calculated
+
+    @property
+    def durations(self):
+        return self._durations
+    @durations.setter
+    def durations(self, value):
+        self._error = None # Invalidate the error calculation
+        self._durations = value
+
+    @property
+    def env_name(self):
+        return self._env_name
+
+    @property
+    def error(self):
+        if len(self.durations) == 0: return None
+        return self._error or \
+            wasserstein_distance(self.durations, get_durations(self.env_name))
 
     @staticmethod
     def from_episodes(records: list[EpisodeRecord], env_name: str):
@@ -309,18 +328,7 @@ class DurationInfo:
         # Drop the last empty duration
         durations = torch.Tensor(durations[:-1])
 
-        # Return None if there are no durations
-        if len(durations) == 0: return None
+        return DurationInfo(durations.numpy(), env_name)
 
-        # Calculate histogram and distance to the expected histogram for the given game
-        histogram = torch.histogram(durations, BINS).hist
-        histogram = histogram / histogram.sum()
-        error = wasserstein_distance(durations, get_durations(env_name))
-
-        return DurationInfo(
-            error,
-            durations.mean().item(),
-            durations.median().item(),
-            histogram.numpy().tolist(),
-            durations.numpy(),
-        )
+    def __array__(self):
+        return self._durations