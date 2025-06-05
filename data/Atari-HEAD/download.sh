fdd5342d5c7ea5fb99427c0408bab552eaffb3e6
Niko <niko.konzack@t-online.de>
2025-06-05 20:42:01 +0200

    Add Atari-HEAD download script

---
+++ data/Atari-HEAD/download.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Define the base directory where your zip files are located
+BASE_DIR="data/Atari-HEAD"
+
+echo Downloading Atari-HEAD
+curl -L https://zenodo.org/api/records/3451402/files-archive -o head.zip
+unzip head.zip -d $BASE_DIR
+rm head.zip
+
+# Loop through each .zip file in the specified directory
+game_dirs="["
+for zip_file_path in "$BASE_DIR"/*.zip; do
+  # Unzip the contents into the new directory
+  echo Unzipping $zip_file_path
+  unzip "$zip_file_path" -d "$BASE_DIR"
+  
+  # Delete the original zip file if extraction was successful
+  if [ $? -eq 0 ]; then # Check if the last command (unzip) was successful
+    rm "$zip_file_path"
+  else
+    echo "Error unzipping $zip_file_path. Not deleting original."
+  fi
+
+  # Untar all the trial files
+  game_name=$(basename "$zip_file_path")
+  game_name="${game_name%.zip}"
+  game_dirs+="'$BASE_DIR/$game_name',"
+  for trial_tar in "$BASE_DIR/$game_name"/*.tar.bz2; do
+    echo $trial_tar
+    tar -xjf "$trial_tar" -C "$BASE_DIR/$game_name"
+
+    # Delete the original zip file if extraction was successful
+    if [ $? -eq 0 ]; then # Check if the last command (unzip) was successful
+        rm "$trial_tar"
+    else
+        echo "Error unzipping $trial_tar. Not deleting original."
+    fi
+  done
+done
+
+# Tranform .txt files to .csv
+game_dirs=${game_dirs::-1}
+game_dirs+="]"
+python -c "from atari_cr.atari_head.utils import transform_to_proper_csv; [transform_to_proper_csv(name) for name in $game_dirs]"
